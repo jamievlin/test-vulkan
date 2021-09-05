415afe794b4817f4525de15ed308dfbd8ff7b99d
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2021-09-04 18:52:50 -0600

    CXX: Expose native GLFW and Vulkan functions.

--- common.h
+++ common.h
@@ -2,10 +2,16 @@
 
 #if defined(_WIN32)
 #include <iso646.h>
+#define VK_USE_PLATFORM_WIN32_KHR
 #endif
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#if defined(_WIN32)
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+#endif
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
