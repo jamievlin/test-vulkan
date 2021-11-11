136eeae83a1c5135f06eba1c026868fc261f9e1c
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2021-11-10 20:17:31 -0700

    CXX: Add return code to CHECK_VK_SUCCESS.

--- include/common.h
+++ include/common.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #if defined(_WIN32)
+
 #include <iso646.h>
+
 #define VK_USE_PLATFORM_WIN32_KHR
 #endif
 
@@ -10,13 +12,16 @@
 
 #if defined(_WIN32)
 #define GLFW_EXPOSE_NATIVE_WIN32
+
 #include <GLFW/glfw3native.h>
+
 #endif
 
 // glm
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_LEFT_HANDED
+
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 
@@ -27,6 +32,7 @@
 #include <memory>
 #include <vector>
 #include <array>
+#include <map>
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
@@ -43,26 +49,28 @@ using std::nullopt;
 #include "utils.h"
 #include "ErrorMessages.h"
 
-#if defined(_WIN32) && defined(DEBUG)
-#define PRINT_ERR_MSG(msg) std::cerr << "Error message: " << (msg) << std::endl;
-#else
-// already printed out in linux
-#define PRINT_ERR_MSG(msg)
-#endif
-
 #if defined(DEBUG)
-#define PRINT_ERROR_LINE() std::cerr << "Runtime error at " << __FILE__ << ":" << __LINE__ << std::endl
+#define PRINT_ERR_MSG(msg) std::cerr << "Error message: " << (msg) << std::endl;
+#define PRINT_ERROR_LINE() std::cerr << "Runtime error at " << __FILE__ << ":" << __LINE__ << std::endl;
+#define PRINT_RET_CODE(err) std::cerr << "Return code: " << (err) << std::endl
 #else
+#define PRINT_ERR_MSG(msg)
 #define PRINT_ERROR_LINE()
+#define PRINT_RET_CODE(err)
 #endif
 
-#define CHECK_VK_SUCCESS(fn, msg) if ((fn) != VK_SUCCESS) { \
+#define CHECK_VK_SUCCESS(fn, msg) { \
+    VkResult __retcode = (fn);                                                     \
+    if (__retcode != VK_SUCCESS) {\
     PRINT_ERROR_LINE();                                     \
-    PRINT_ERR_MSG(msg);                                     \
-    throw std::runtime_error(msg);                          \
-    }
+    PRINT_ERR_MSG(msg);                                                            \
+    PRINT_RET_CODE(__retcode);         \
+    assert(__retcode == VK_SUCCESS);                        \
+    }}
 
 #define USE_HLSL 1
 #define VK_API_VERSION VK_API_VERSION_1_2
 
 #define VEC4_ALIGN alignas(sizeof(glm::vec4))
+
+constexpr size_t MAX_FRAMES_IN_FLIGHT = 3;
